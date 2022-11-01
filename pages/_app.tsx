f81c921156f83e5bb95c91810cc892cec23a6984
Avinash Kumar <avinashkmr886@gmail.com>
2022-11-01 16:35:59 +0530

    changed css precedence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { getLayout } from "../components/Layout";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+const cache = createCache({
+  key: "css",
+  prepend: true,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+  return (
+    <CacheProvider value={cache}>
+      {getLayout(<Component {...pageProps} />)}
+    </CacheProvider>
+  );
 }
