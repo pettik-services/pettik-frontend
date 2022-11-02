ef75b1b6bfec00bc78be7d9e707c7bdaa6747716
Avinash Kumar <avinashkmr886@gmail.com>
2022-11-02 12:44:23 +0530

    new changes

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiServerProxyUrl = process.env.API_SERVER || 'https://6u26pb8qe.execute-api.us-east-1.amazonaws.com';
+const apiServerProxyUrl = process.env.API_SERVER || 'https://6u26pb8q2e.execute-api.us-east-1.amazonaws.com';
 
 export const getBlogs = () =>
     axios
