127b83bf25ea5b4c06c9512454c24f7f85e2fb1d
Avinash Kumar <avinashkmr886@gmail.com>
2022-11-14 21:25:18 +0530

    added loader image

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
+import Image from "next/image";
+import LoaderGif from "../assets/images/loader.gif";
 
 export default function Loader() {
   return (
-    <div className="h-screen w-full">
+    <div className='h-screen w-full'>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={true}>
-        <CircularProgress color='inherit' />
+        <Image src={LoaderGif} height={200} width={200} alt={"loader"} />
       </Backdrop>
     </div>
   );
