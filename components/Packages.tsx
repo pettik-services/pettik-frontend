310dae0882149c318937a66355ca59aa77de5fe0
Avinash Kumar <avinashkmr886@gmail.com>
2022-11-14 22:04:33 +0530

    new changes

--- components/Packages.tsx
+++ components/Packages.tsx
@@ -72,7 +72,7 @@ export const PackageItem: React.FC<ItemProps> = ({
       </div>
       <div className='flex flex-col w-full gap-y-8'>
         <div className='flex flex-row w-full font-nunito-black text-xl md:text-xl'>
-          <div className='w-1/2 flex  ml-6 justify-start'>{price}</div>
+          <div className='w-1/2 flex  ml-6 justify-start'>₹ {price}</div>
           <div className='w-1/2 flex justify-end mr-6'>{duration}</div>
         </div>
         {buttonEnabled && (
